e4da8026745fbea48b2c5f3fab0bb6f52b4bcf34
MSVenkat1010 <kmanjusai.venkat@gmail.com>
2023-11-29 23:24:48 +0530

    #Register and modification in user Login

--- src/userLogin/Register/Register.js
+++ src/userLogin/Register/Register.js
@@ -1,28 +1,29 @@
 import React, { useEffect, useRef } from "react";
-import "./userLogin.css";
-import insta_Png from ".././Resources/Instagram_SideBar.png"
+import "./Register.css";
 import RemoveRedEyeOutlinedIcon from "@mui/icons-material/RemoveRedEyeOutlined";
 
 import TextField from "@mui/material/TextField";
 import { Button, FormControl } from "@mui/material";
 
-function UserLogin() {
+function Register() {
   const [showPassword, setShowPassword] = React.useState(false);
+  const [showRePassword, setShowRePassword] = React.useState(false);
   const [userDetails, setUserDetails] = React.useState({
-    userName: "",
-    passWord: "",
+    email:"",
+    userName:"",
+    passWord:"",
+    rePassWord:""
   });
 
-  const handleClickShowPassword = () => {
-    setShowPassword((show) => !show);
-  };
-  const handleUserLogin =(event,field) => {
+  const handleRegister =(event,field) => {
 
-    if ((field === "userName" || field === "passWord") ){
+    if ((field === "userName" || field === "passWord" ||field === "rePassWord" )) {
       if(event.target.value.length <= 10){setUserDetails({ ...userDetails, [field]: event.target.value });}
+    }else{
+      setUserDetails({ ...userDetails, [field]: event.target.value });
     }
   };
- 
+
 
   const handleMouseDownPassword = (
     event: React.MouseEvent<HTMLButtonElement>
@@ -30,40 +31,44 @@ function UserLogin() {
     event.preventDefault();
   };
   return (
-    <div className="userLogin full-screen-div" style={{ color: "white" }}>
-      <div className="bannerLogin">
-      <img
-          className="instagram_png"
-          src={insta_Png}
-          alt="Instagram Logo"
-        />
-      </div>
+    <div className="Register full-screen-div" style={{ color: "white" }}>
       <div className="credLogin">
         <img
           className="instagram_logo"
           src="https://freelogopng.com/images/all_img/1658587465instagram-name-logo.png"
           alt="Instagram Logo"
         />
+                <TextField
+             //   autoComplete="email"
+          label="Email address"
+          value={userDetails.email}
+         onChange={(event)=>{handleRegister(event,"email")}}
+          type={"email"}
+          id="email"
+          sx={{ m: 1, width: "25ch" }}
+          placeholder="email addresss"
+        />
         <TextField
-          label="user Name"
+          label="User name"
           value={userDetails.userName}
-          onChange={(event)=>{handleUserLogin(event,"userName")}}
-          id="outlined-start-adornment"
+         onChange={(event)=>{handleRegister(event,"userName")}}
+          type={"text"}
+          id="userName"
           sx={{ m: 1, width: "25ch" }}
           placeholder="user name"
         />
         <TextField
           label="password"
           value={userDetails.passWord}
-          id="test"
+          id="password"
           type={showPassword ? "text" : "password"}
           placeholder="password"
           sx={{ m: 1, width: "25ch" }}
-          onChange={(event)=>{handleUserLogin(event,"passWord")}}
+         onChange={(event)=>{handleRegister(event,"passWord")}}
           InputProps={{
             endAdornment: (
               <span
-                onClick={handleClickShowPassword}
+                onClick={()=>{setShowPassword(!showPassword)}}
                 onMouseDown={handleMouseDownPassword}
               >
                 <RemoveRedEyeOutlinedIcon />
@@ -71,19 +76,26 @@ function UserLogin() {
             ),
           }}
         />
-        <FormControl>
-          <Button
-            variant="contained"
-            sx={{ m: 1, width: "25ch", textTransform: "none" , backgroundColor: "rgb(87,164,248)" }}
-            disableRipple
-          >
-            {" "}
-            Log in
-          </Button>
-        </FormControl>
-        <div className="orElement">
-          <hr /> or <hr />{" "}
-        </div>
+                <TextField
+          label="re-enter password"
+          value={userDetails.rePassWord}
+          id="rePassword"
+          type={showRePassword ? "text" : "password"}
+          placeholder="password"
+          sx={{ m: 1, width: "25ch" }}
+          onChange={(event)=>{handleRegister(event,"rePassWord")}}
+          InputProps={{
+            endAdornment: (
+              <span
+                onClick={()=>{setShowRePassword(!showRePassword)}}
+                onMouseDown={handleMouseDownPassword}
+              >
+                <RemoveRedEyeOutlinedIcon />
+              </span>
+            ),
+          }}
+        />
+
         <FormControl>
           <Button
             variant="contained"
@@ -94,10 +106,10 @@ function UserLogin() {
             Register
           </Button>
         </FormControl>
-        <span className="forgotPassword" >Forgotten your password?</span>
+
       </div>
     </div>
   );
 }
 
-export default UserLogin;
+export default Register;
